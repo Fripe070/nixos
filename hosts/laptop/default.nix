7b66cb1ea22033a5404dedf1f68456db0fd43692
Fripe <72686066+Fripe070@users.noreply.github.com>
2026-05-20 22:18:28 +0000

    Finger print setup

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -20,4 +20,31 @@
       monitor = ["eDP-1, 1920x1080@144, 0x0, 1.25"];
     };
   };
+  
+  services = {
+    power-profiles-daemon.enable = true;
+  };
+
+  # Patch libfprint with support for the reader on my laptop
+  nixpkgs.overlays = [
+    (final: prev: {
+      libfprint = prev.libfprint.overrideAttrs (oldAttrs: {
+        # Replace entirely with the PR branch
+        src = final.fetchgit {
+          url = "https://gitlab.freedesktop.org/libfprint/libfprint.git";
+          rev = "refs/merge-requests/554/head";
+          hash = "sha256-/DhkBcDe/Jg5MrcK99vvlNHgnKbZd/ikTxZmPqkqAVM="; 
+        };
+        # no default nixpkgs patches 
+        patches = [];
+        # no tests 
+        doCheck = false;
+      });
+    })
+  ];
+  services.fprintd.enable = true;
+  security.pam.services = {
+    sudo.fprintAuth = true;
+    login.fprintAuth = true;
+  };
 }
