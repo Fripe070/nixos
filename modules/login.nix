ff05be43bf0db1f2fee2441c5f3e9fd976000957
Fripe <72686066+Fripe070@users.noreply.github.com>
2026-06-11 13:49:08 +0000

    Launch ly on TTY7

--- modules/login.nix
+++ modules/login.nix
@@ -1,7 +1,9 @@
+{ lib, ... }:
 {
   services.displayManager.ly = {
     enable = true;
     settings = {
+      tty = lib.mkForce 7;
       bigclock = true;
       blank_password = true;
       hide_borders = true;
