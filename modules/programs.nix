a604db74d72be3bb74d1c44987eb605d90a494e1
Fripe <72686066+Fripe070@users.noreply.github.com>
2026-09-02 18:20:41 +0000

    set editor and terminal env vars

--- modules/programs.nix
+++ modules/programs.nix
@@ -2,17 +2,31 @@
 {
   home-manager.users.${identity.username} = { pkgs, ... }: {
     home.packages = with pkgs; [
-      vscode
       firefox
       # File manager
       kdePackages.dolphin
+
+      vscode
     ];
+    
+    home.sessionVariables = {
+      EDITOR = "nvim";
+      VISUAL = "code";
+      TERMINAL = "kitty";
+    };
 
     programs.kitty = {
       enable = true;
       settings = {
         confirm_os_window_close = 0;
       };
+    };
+
+    programs.nixvim = {
+      enable = true;
+
+      colorschemes.catppuccin.enable = true;
+      plugins.lualine.enable = true;
     };
   };
 }
