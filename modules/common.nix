2a65d57daab132f70c52e7f8bbefdc661a61b706
Fripe <72686066+Fripe070@users.noreply.github.com>
2026-09-10 11:22:51 +0000

    Trusted users

--- modules/common.nix
+++ modules/common.nix
@@ -18,6 +18,7 @@
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
     ];
+    trusted-users = [ "root" "@wheel" ];
   };
 
   boot.loader = {
